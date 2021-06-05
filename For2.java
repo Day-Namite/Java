ba9ab6b73b11e43b7a5247b9512affd183ea7d4c
Day-Namite <daisybarbosa.p@gmail.com>
2021-06-04 23:11:11 -0300

    Exercicios for, while e do while

---
+++ For2.java
@@ -0,0 +1,19 @@
+package GenEx;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1- Informar todos os números de 1000 a 1999 que quando divididos por 11 obtemos resto = 5. (FOR)
+		int n;
+		for (n=1000;n<=1999;n++)
+		{
+			if (n%11==5)
+			{
+				System.out.println(n);
+			}
+			}
+
+	}
+
+}
